0411981428e1bb0ef7715747c3e19f0f28856f66
MGXD <3508788007@qq.com>
2026-09-26 18:10:12 +0800

    docs: update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.magicnote.mgxd"
         minSdk = 24
         targetSdk = 35
-        versionCode = 74
-        versionName = "7.4"
+        versionCode = 75
+        versionName = "7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
